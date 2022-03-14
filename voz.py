caaf84f07c7e5f6b74ae5916fe8e65f2e509b5bc
Rafael Silva <88403845+orafinhasilva@users.noreply.github.com>
2022-03-14 09:48:15 -0300

    Update voz.py

--- voz.py
+++ voz.py
@@ -3,7 +3,7 @@ import pyttsx3
 # Faz o carregamento da lib 
 engine = pyttsx3.init()
 
-# Texto em que a maquina vai ler
+# Converte o texto inserido no campo em áudio
 ads = engine.say(input('Digite o que você deseja falar: '))
 
 # Executa a leitura do texto #
